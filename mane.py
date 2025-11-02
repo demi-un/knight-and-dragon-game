f6e837d59cf72d119c5f23e57f1bda604db2c244
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-02 20:08:07 +0500

    work version

--- mane.py
+++ mane.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class Person:
@@ -7,6 +8,7 @@ class Person:
         self.hp = hp
         self.damage = damage
         self.protection = protection
+
         self.selected_body_part = selected_body_part
         self.unprotected_part = unprotected_part
 
@@ -21,7 +23,6 @@ class Person:
         return damage
 
     def attack(self, target):
-        # target.got_damage(self.damage)
         damage = self.damage
 
         target.unprotected_part = random.choice(['h', 'b', 'l'])
@@ -31,10 +32,10 @@ class Person:
         else:
             damage *= 0.75
 
-        damage = target.got_damage()
+        damage = target.got_damage(damage)
 
         print(f'{self.name} attacks {target.name} with {self.damage} damage')
-        print(f"The damage, taking into account the opponent's defense, was {damage}")
+        print(f"The damage, taking into account the opponent's defense and critical damage, was {damage}")
         if target.is_alive():
             print(f'{target.name} HP: {target.hp}')
         else:
@@ -53,44 +54,76 @@ class Knight(Person):
         self.regeneration = regeneration
 
     def attack(self, target):
-        super().attack(target)
-        if self.is_alive():
+        damage = self.damage
+
+        target.unprotected_part = random.choice(['h', 'b', 'l'])
+
+        if self.selected_body_part == target.unprotected_part:
+            damage *= 1.25
+        else:
+            damage *= 0.75
+
+        damage = target.got_damage(damage)
+
+        print(f'{self.name} attacks {target.name} with {self.damage} damage')
+        print(f"The damage, taking into account the opponent's defense and critical damage, was {damage}")
+
+        if self.is_alive() and random.random() <= 0.3:
             self.hp += self.regeneration
             print(f'{self.name} restored {self.regeneration} HP')
+
+        if target.is_alive():
+            print(f'{target.name} HP: {target.hp}')
+        else:
+            print(f'{target.name} died')
 
     def __str__(self):
         return f'{self.name} | HP: {self.hp}, damage: {self.damage}, protection: {self.protection}, regeneration: {self.regeneration}'
 
 
-#
-#
-# class Dragon(Person):
-#     def __init__(self, name, hp, damage, protection, fire_damage, burning_time):
-#         super().__init__(name, hp, damage, protection)
-#         self.fire_damage = fire_damage
-#         self.burning_time = burning_time
-#
-#     def attack(self, target):
-#         super().attack(target)
-#         target.hp
-#
-#
-# def fight(person_1, person_2):
-#     while person_1.is_alive() and person_2.is_alive():
-#         person_1.attack(person_2)
-#         if person_2.is_alive():
-#             person_2.attack(person_1)
+class Dragon(Person):
+    def __init__(self, name, hp, damage, protection, fire_damage):
+        super().__init__(name, hp, damage, protection)
+        self.fire_damage = fire_damage
+
+    def attack(self, target):
+        damage = self.damage
+
+        if random.random() <= 0.3:
+            damage += self.fire_damage
+            'Дракон поджигает ваше тело'
+
+        target.unprotected_part = random.choice(['h', 'b', 'l'])
+
+        if self.selected_body_part == target.unprotected_part:
+            damage *= 1.25
+        else:
+            damage *= 0.75
+
+        damage = target.got_damage(damage)
+
+        print(f'{self.name} attacks {target.name} with {self.damage} damage')
+        print(f"The damage, taking into account the opponent's defense and critical damage, was {damage}")
+        if target.is_alive():
+            print(f'{target.name} HP: {target.hp}')
+        else:
+            print(f'{target.name} died')
+
+    def __str__(self):
+        return f'{self.name} | HP: {self.hp}, damage: {self.damage}, protection: {self.protection}, fire damage: {self.fire_damage}'
 
 
 def fight(person_1, person_2):
     print(f'>>> Fight: {person_1.name} vs {person_2.name} <<<')
+    print('-'*40)
     print(person_1)
     print(person_2)
+    print('-'*40)
     print(f'{person_2.name} будет защищать две части тела из трех. чтобы нанести максимальный урон, '
           'вам нужно предугадать часть тела, которую он не будет защищать')
-    print('Введите часть тела, в которую будете атаковать')
 
     while person_1.is_alive and person_2.is_alive:
+        print('Введите часть тела, в которую будете АТАКОВАТЬ')
         selected_body_part = input("'h': Head, 'b': Body, 'l': Legs >>> ")
 
         print(f'{person_2.name} не защитил {person_2.unprotected_part}')
@@ -98,15 +131,27 @@ def fight(person_1, person_2):
 
         person_1.attack(person_2)
 
+        time.sleep(1)
+        print(f'{person_2.name} атакует {person_1.name}')
+        print('Выберите часть тела которую НЕ будете ЗАЩИЩАТЬ')
+        unprotected_part = input("'h': Head, 'b': Body, 'l': Legs >>> ")
+        person_1.unprotected_part = unprotected_part
+
+        person_2.selected_body_part = random.choice(['h', 'b', 'l'])
+
+        print(f'{person_2.name} атакует {person_1.name} в часть тела {person_2.selected_body_part}')
+        person_2.attack(person_1)
+
+        print('-' * 40)
         print(person_1)
         print(person_2)
+        print('-' * 40)
 
         print('>>>>>>>>>>>>>>------------<<<<<<<<<<<<<<<<<')
 
 slime = Person('slime', 100, 10, 5)
-knight = Knight('knight', 250, 20, 5, 1)
 
-fight(knight, slime)
+dragon = Dragon('dragon', 400, 15, 3, 30)
+knight = Knight('knight', 250, 20, 5, 35)
 
-
-# лес замок
+fight(knight, dragon)
---
+++ mane_3.py
@@ -0,0 +1,171 @@
+import random
+import time
+
+
+class Person:
+    def __init__(self, name, hp, damage, protection, selected_body_part='h', unprotected_part='h'):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        self.protection = protection
+        self.selected_body_part = selected_body_part
+        self.unprotected_part = unprotected_part
+
+    def got_damage(self, damage):
+        real_damage = round(damage * (100 / (100 + self.protection)))
+        if real_damage < 1:
+            real_damage = 1
+        self.hp -= real_damage
+        return real_damage
+
+    def attack(self, target):
+        # Выбор незащищённой части тела противника
+        target.unprotected_part = random.choice(['h', 'b', 'l'])
+
+        # Проверка попадания в незащищённое место
+        if self.selected_body_part == target.unprotected_part:
+            damage = self.damage * 1.25  # крит
+        else:
+            damage = self.damage * 0.75  # противник защитился
+
+        damage = target.got_damage(damage)
+
+        print(f'{self.name} атакует {target.name} с уроном {self.damage}')
+        print(f"урон с учётом защиты и крита: {damage}")
+        if target.is_alive():
+            print(f'{target.name} HP: {target.hp}\n')
+        else:
+            print(f'{target.name} погиб\n')
+
+    def is_alive(self):
+        return self.hp > 0
+
+    def __str__(self):
+        return f'{self.name} | HP: {self.hp}, Урон: {self.damage}, Защита: {self.protection}'
+
+
+class Knight(Person):
+    def __init__(self, name, hp, damage, protection, regeneration, selected_body_part='h', unprotected_part='h'):
+        super().__init__(name, hp, damage, protection, selected_body_part, unprotected_part)
+        self.regeneration = regeneration
+
+    def attack(self, target):
+        target.unprotected_part = random.choice(['h', 'b', 'l'])
+
+        if self.selected_body_part == target.unprotected_part:
+            damage = self.damage * 1.25
+        else:
+            damage = self.damage * 0.75
+
+        damage = target.got_damage(damage)
+
+        print(f'{self.name} атакует {target.name} с уроном {self.damage}')
+        print(f"урон с учётом защиты и крита: {damage}")
+
+        # 30% шанс восстановить здоровье
+        if self.is_alive() and random.random() <= 0.3:
+            self.hp += self.regeneration
+            print(f'{self.name} восстановил {self.regeneration} HP')
+
+        if target.is_alive():
+            print(f'{target.name} HP: {target.hp}\n')
+        else:
+            print(f'{target.name} погиб\n')
+
+    def __str__(self):
+        return f'{self.name} | HP: {self.hp}, Урон: {self.damage}, Защита: {self.protection}, Регенерация: {self.regeneration}'
+
+
+class Dragon(Person):
+    def __init__(self, name, hp, damage, protection, fire_damage):
+        super().__init__(name, hp, damage, protection)
+        self.fire_damage = fire_damage
+
+    def attack(self, target):
+        damage = self.damage
+
+        # 30% шанс добавить огненный урон
+        if random.random() <= 0.3:
+            damage += self.fire_damage
+            print('Дракон поджигает противника!')
+
+        target.unprotected_part = random.choice(['h', 'b', 'l'])
+
+        if self.selected_body_part == target.unprotected_part:
+            damage *= 1.25
+        else:
+            damage *= 0.75
+
+        damage = target.got_damage(damage)
+
+        print(f'{self.name} атакует {target.name} с уроном {self.damage}')
+        print(f"урон с учётом защиты и крита: {damage}")
+        if target.is_alive():
+            print(f'{target.name} HP: {target.hp}\n')
+        else:
+            print(f'{target.name} погиб\n')
+
+    def __str__(self):
+        return f'{self.name} | HP: {self.hp}, Урон: {self.damage}, Защита: {self.protection}, Огненный урон: {self.fire_damage}'
+
+
+def fight(person_1, person_2):
+    print(f'>>> Бой: {person_1.name} vs {person_2.name} <<<')
+    print('-' * 40)
+    print(person_1)
+    print(person_2)
+    print('-' * 40)
+
+    while person_1.is_alive() and person_2.is_alive():
+        print('\nВыберите часть тела, в которую будете АТАКОВАТЬ:')
+        selected_body_part = input("'h': Голова, 'b': Тело, 'l': Ноги >>> ").strip().lower()
+
+        if selected_body_part not in ['h', 'b', 'l']:
+            print('Неверный ввод')
+            continue
+
+        person_1.selected_body_part = selected_body_part
+        person_1.attack(person_2)
+
+        if not person_2.is_alive():
+            print(f'\n{person_2.name} пал в бою! Победил {person_1.name}!')
+            break
+
+        time.sleep(1)
+
+        print(f'{person_2.name} готовит атаку...')
+        print('Выберите часть тела, которую НЕ будете защищать:')
+        unprotected_part = input("'h': Голова, 'b': Тело, 'l': Ноги >>> ").strip().lower()
+
+        if unprotected_part not in ['h', 'b', 'l']:
+            print('Неверный ввод')
+            continue
+
+        person_1.unprotected_part = unprotected_part
+        person_2.selected_body_part = random.choice(['h', 'b', 'l'])
+        print(f'{person_2.name} атакует в {person_2.selected_body_part}')
+        person_2.attack(person_1)
+
+        if not person_1.is_alive():
+            print(f'\n{person_1.name} пал в бою! Победил {person_2.name}!')
+            break
+
+        print('-' * 40)
+        print(person_1)
+        print(person_2)
+        print('-' * 40)
+
+
+dragon = Dragon('Дракон', 120, 15, 3, 20)
+knight = Knight('Рыцарь', 80, 18, 4, 10)
+
+fight(knight, dragon)
+
+Betty_Botter_bought_some_butter___Betty_Botter_bought_some_better_butter = random.randint(1, 2)
+if knight.is_alive():
+    print('Рыцарь защитил королевство')
+else:
+    if Betty_Botter_bought_some_butter___Betty_Botter_bought_some_better_butter == 1:
+        print('Дракон сжег королевство')
+    else:
+        print('Дракон пощадил жителей королевства и улетел')
